48842c98d8070a0325bdd4a9802670f889bd8e9c
Veldosya Sir <v.ostapenko.d@gmail.com>
2025-05-06 23:32:30 +0300

    Add CountriesController

---
+++ WebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PromomashTest.Application.DTO;
+using PromomashTest.Application.Queries;
+
+namespace PromomashTest.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CountriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CountryDto>>> Get()
+        {
+            var countries = await _mediator.Send(new GetCountriesQuery());
+            return Ok(countries);
+        }
+    }
+}
